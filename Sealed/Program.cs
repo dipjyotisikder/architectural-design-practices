bfb3565c4768ef03b56880212231d2436bc770bb
Dipjyoti Sikder <dipjyotisikder@gmail.com>
2021-01-07 01:09:38 +0600

    Sealed class added

---
+++ Sealed/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sealed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RESULT IS :
+            //IAmSealed CLASS CAN NOT BE INHERITED BY ANY CLASS EVER
+            //BUT IAmSealed CLASS CAN BE INSTANTIATED
+        }
+    }
+
+
+    public sealed class IAmSealed
+    {
+        public IAmSealed()
+        {
+        }
+    }
+
+    public class ICanNotInheritSealed //: IAmSealed 
+    {
+
+    }
+
+}
